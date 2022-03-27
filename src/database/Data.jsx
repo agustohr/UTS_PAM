0e2c6d0a9b436d02fc8517278c05752e80051044
Agusto Hawlai <64683138+agustohr@users.noreply.github.com>
2022-03-27 11:40:59 +0700

    Add files via upload

---
+++ src/database/Data.jsx
@@ -0,0 +1,51 @@
+const JADWAL = [
+    {
+        jadwal_id: "1",
+        pelabuhanAwal: "Merak",
+        pelabuhanAkhir: "Bakauheni",
+        kelas_layanan: "Reguler", 
+        tanggal_masuk: "26/03/2022",
+        jam_masuk: "13.00 WIB"
+    },
+    {
+        jadwal_id: "2",
+        pelabuhanAwal: "Merak",
+        pelabuhanAkhir: "Bakauheni",
+        kelas_layanan: "Express", 
+        tanggal_masuk: "26/03/2022",
+        jam_masuk: "13.00 WIB"
+    },
+    {
+        jadwal_id: "3",
+        pelabuhanAwal: "Bakauheni",
+        pelabuhanAkhir: "Merak",
+        kelas_layanan: "Reguler", 
+        tanggal_masuk: "27/03/2022",
+        jam_masuk: "15.00 WIB"
+    },
+    {
+        jadwal_id: "4",
+        pelabuhanAwal: "Bakauheni",
+        pelabuhanAkhir: "Merak",
+        kelas_layanan: "Reguler", 
+        tanggal_masuk: "27/03/2022",
+        jam_masuk: "15.00 WIB"
+    },
+    {
+        jadwal_id: "5",
+        pelabuhanAwal: "Bakauheni",
+        pelabuhanAkhir: "Merak",
+        kelas_layanan: "Express", 
+        tanggal_masuk: "27/03/2022",
+        jam_masuk: "18.00 WIB",
+
+        jadwal_id: "6",
+        pelabuhanAwal: "Merak",
+        pelabuhanAkhir: "Bakauheni",
+        kelas_layanan: "Express", 
+        tanggal_masuk: "27/03/2022",
+        jam_masuk: "18.00 WIB"
+    },
+];
+
+export { JADWAL }
